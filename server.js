abbdd6138a7653a0192caafd8ec7b6a68492f714
Kashif Azeemi <61817392+kashifazeemi7@users.noreply.github.com>
2021-10-27 23:05:36 +0500

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/profile', (req, res) => {
+  res.send('Welcome to your profile!')
+})
+
+app.get('/dash', (req, res) => {
+    res.send('Welcome to your dashboard!')
+  })
+
+app.get('/home', (req, res) => {
+    res.send('Welcome to home!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
